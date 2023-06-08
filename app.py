ee67cc367614a82940bb6432f488a1a99db2f49f
6aligula <vinicio.naranjo15@gmail.com>
2023-06-08 13:51:05 +0200

    modify for production in ec2 aws

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ def extract_text_from_pdf(file_path):
     pdf_file_obj = open(file_path, 'rb')
     pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
     text = ""
-    for page_num in range(pdf_reader.numPages):
-        page_obj = pdf_reader.getPage(page_num)
-        text += page_obj.extractText()
+    for page_num in range(len(pdf_reader.pages)):
+        page_obj = pdf_reader.pages[page_num]
+        text += page_obj.extract_text()
     pdf_file_obj.close()
     return text
 
