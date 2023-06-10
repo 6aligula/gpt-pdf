b87e37e8d969f948f2c291ccbf12db928031a8e7
6aligula <vinicio.naranjo15@gmail.com>
2023-06-10 16:15:21 +0200

    add logs and modify index.html for https

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from wtforms import SubmitField
 import openai
 import time 
 from openai.error import RateLimitError
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import os
 
@@ -16,49 +16,55 @@ app = Flask(__name__)
 CORS(app) 
 socketio = SocketIO(app)
 
-pdf_text = ""  # variable global para almacenar el texto del PDF
+app.debug = False
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    file_handler = RotatingFileHandler('flask_app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+
+pdf_text = ""
 
 def generate_summary_with_retry(messages):
-    wait_time = 5  # Tiempo de espera inicial
+    wait_time = 5
     while True:
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=messages,
             )
-            #print("API request successful")  # Debug point
-            #print("Response:", response)  # New debug point
 
             return response
         except RateLimitError:
-            #print(f"Rate limit exceeded. Retrying in {wait_time} seconds...")
+            app.logger.warning(f"Se excedió el límite de tasa. Reintentando en {wait_time} segundos...") 
             time.sleep(wait_time)
-            wait_time *= 2  # Aumenta el tiempo de espera para el próximo intento
+            wait_time *= 2
 
 @socketio.on('generate_summary')
 def handle_generate_summary(prompt):
     summaries = []
-    max_length = 1000  # ajusta este valor según sea necesario
-    #print('prompt: ', prompt)
-    # divide el texto en secciones
+    max_length = 1000
     sections = [pdf_text[i:i + max_length] for i in range(0, len(pdf_text), max_length)]
-    #print(f"Number of sections: {len(sections)}")  # Debug point
 
     for i, section in enumerate(sections):
         messages = [
             {"role": "system", "content": "Tu eres un amable asistente"},
             {"role": "user", "content": f"{prompt} de : {section}"}
         ]
-        print(messages)
-        response = generate_summary_with_retry(messages)  # Usa la nueva función con reintento
+        response = generate_summary_with_retry(messages)
 
         summary = response.choices[0].message["content"]
         summaries.append(summary)
-        print(f"Generated summary for section {i + 1}")  # Debug point
+        app.logger.info(f"Generando resumen para la sección {i + 1}") 
         
-        # emitir el resumen a medida que se genera
         emit('new_summary', summary)
-
+    
+    app.logger.info(f"Resumen finalizado") 
     emit('new_summary', "finish")
     return " ".join(summaries)
 
@@ -75,25 +81,34 @@ def extract_text_from_pdf(file_path):
 @app.route('/upload', methods=['POST'])
 def upload():
     global pdf_text
+    client_ip = request.headers.get('X-Real-IP', request.remote_addr)
     if request.method == 'POST':
         try: 
             pdf = request.files['pdf']
             filename = secure_filename(pdf.filename)
-            #print(f"File name: {filename}")  # Debug point
+            app.logger.info(f"Guardando el archivo: {filename} del cliente: {client_ip}")
             pdf.save(filename)
             pdf_text = extract_text_from_pdf(filename)
-            #print(f"Text length: {len(pdf_text)}")  # Debug point
+            app.logger.info(f"Longitud del texto extraído: {len(pdf_text)}") 
             if pdf_text:
                 socketio.emit('pdf_processing_done', "El PDF ha sido procesado correctamente. Ahora procede a escribir tu peticion a GPT3.5-Turbo")
-                #print("pdf lleno ")
         except Exception as e:
             socketio.emit('pdf_processing_error', str(e))
+            app.logger.error(f"Error al procesar el PDF: {str(e)}")
     return redirect(url_for('home'))
 
 @app.route('/')
 def home():
+    x_forwarded_for = request.headers.get('X-Forwarded-For')
+    if x_forwarded_for:
+        ip_addresses = x_forwarded_for.split(',')
+        client_ip = ip_addresses[0]
+    else:
+        client_ip = request.remote_addr
+
+    app.logger.info(f"Rendering home page, cliente: {client_ip}")
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.logger.info('Starting Flask app')
+    socketio.run(app, debug=False)
